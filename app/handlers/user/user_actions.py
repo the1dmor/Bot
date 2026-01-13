f4e6cb579f527e9682fab22ef8b4499e2657208e
the1dmor <programmer008572@gmail.com>
2026-01-13 14:05:21 +0500

    Delete app/handlers/user/user_actions.py

--- app/handlers/user/user_actions.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-
-router = Router()
-# Placeholder: User uchun asosiy handlerlarni qo'shing, masalan:
-# @router.message(Command("start"))
-# async def start_user(message: Message):
-#     await message.reply("Salom, foydalanuvchi!")
