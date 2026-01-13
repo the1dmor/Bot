07a06a5d7bdde7a023339b65214d5ed9e6b2207b
the1dmor <programmer008572@gmail.com>
2026-01-13 13:32:03 +0500

    Add files via upload

---
+++ app/funcs/languages.py
@@ -0,0 +1,35 @@
+def act_1_lang():
+    return (
+        "🍡• ─── ✿ <b>Kiseki</b> ✿ ─── •🍡\n\n"
+        "🟢 <b>SYSTEM ONLINE</b>\n"
+        "👤 User detected: <i>Otaku-san</i>\n\n"
+        "🔍 Qidiruv rejimi: <b>TURBO</b>\n"
+        "🍧 Event & bonuslar: <b>OCHIQ</b>\n\n"
+        "🍙 <b>Developer:</b> @MikioNatsu"
+    )
+
+
+def act_2_lang():
+    return (
+        ". .  ── •✧⚙️✧• ──  . .\n\n"
+        "<b>👔 Admin panelga xush kelibsiz!</b>\n\n"
+        "🛠 Bu yerda siz:\n"
+        "• Anime qo‘shasiz\n"
+        "• Qismlar boshqarasiz\n"
+        "• Post joylaysiz"
+    )
+
+
+def act_3_lang():
+    text = "🔘Sizda hali guruhlar mavjud emas !"
+    return text
+
+
+def act_4_lang():
+    text = "<b>📌Kerakli guruhni tanlang 👇🏻</b>"
+    return text
+
+
+def act_5_lang(name):
+    text = f"💬<b>{name}</b> - guruhi <b>\n\n🗒Kerakli bo'limni tanlang 👇🏻</b>"
+    return text
